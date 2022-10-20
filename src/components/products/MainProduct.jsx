bd709da58d160aeb09751047b5271f5c0287ee1e
totigorbea <tomasgorbea@gmail.com>
2022-10-20 20:30:24 -0300

    primer commit

---
+++ src/components/products/MainProduct.jsx
@@ -0,0 +1,29 @@
+const MainProduct = () => {
+    return (
+        <main>
+        <section>Galeria de imagenes</section>
+        <section>
+          <p>Sneaker Company</p>
+          <h2>Sneakers de edicion limtiada</h2>
+          <p>
+            
+Estas zapatillas de deporte de perfil bajo son tu compañero de ropa informal perfecto. Con una suela exterior de goma duradera, resistirán todo lo que el clima pueda ofrecer.
+          </p>
+          <p>
+            <span>$12500.00</span>
+            <span>50%</span>
+          </p>
+          <p>$250.00</p>
+          <div>
+            <div>
+              <button>-</button>
+              <span>0</span>
+              <button>+</button>
+            </div>
+            <button>Agregar al carrito</button>
+          </div>
+        </section>
+      </main>
+    )
+}
+export default MainProduct;
